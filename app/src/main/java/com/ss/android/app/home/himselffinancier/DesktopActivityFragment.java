eefb4c61eeaa38d890f3ce7910d21e344e039014
StepyginSergey <stepygin.sergey@gmail.com>
2018-09-08 07:19:37 +0300

    Создание скелета проекта для мобильного приложения  Сам Себе Финансист

---
+++ app/src/main/java/com/ss/android/app/home/himselffinancier/DesktopActivityFragment.java
@@ -0,0 +1,22 @@
+package com.ss.android.app.home.himselffinancier;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DesktopActivityFragment extends Fragment {
+
+    public DesktopActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_desktop, container, false);
+    }
+}
